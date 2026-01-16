38cd728949a8cf7078094fc4811ba91af0add27f
unDefFtr <tanqf7@163.com>
2026-01-17 02:59:12 +0800

    chore: 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycses',
-    version='0.1.2',
+    version='0.1.3',
     author='SmartTeachCN',
     author_email='contact@smart-teach.cn',
     description='CSES access framework for Python',
